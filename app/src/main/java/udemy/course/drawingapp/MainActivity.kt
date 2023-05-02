fd4fc19f2172f4db672cd59a5e765520d07f13b9
Luka Ilić <lukailic100@gmail.com>
2023-05-02 23:04:35 +0200

    added loading background image from gallery

--- app/src/main/java/udemy/course/drawingapp/MainActivity.kt
+++ app/src/main/java/udemy/course/drawingapp/MainActivity.kt
@@ -1,17 +1,61 @@
 package udemy.course.drawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var currentColorButtonSelected: ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK && result.data != null) {
+                val imageBkg: ImageView = findViewById(R.id.bkg_image)
+                imageBkg.setImageURI(result.data?.data)
+            }
+        }
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val permissionGranted = it.value
+
+                if (permissionGranted) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Read ext storage - granted",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    val pickImageIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickImageIntent)
+                } else {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Read ext storage - NOT granted",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +75,9 @@ class MainActivity : AppCompatActivity() {
 
         val brushSizeBtn: ImageButton = findViewById(R.id.brush_size_btn)
         brushSizeBtn.setOnClickListener { brushSizeDialog() }
+
+        val galleryBtn: ImageButton = findViewById(R.id.gallery_btn)
+        galleryBtn.setOnClickListener { requestStoragePermission() }
     }
 
     private fun brushSizeDialog() {
@@ -74,6 +121,38 @@ class MainActivity : AppCompatActivity() {
                 )
             )
             currentColorButtonSelected = view
+        }
+    }
+
+    private fun customProgressDialog() {
+        val customProgressDialog = Dialog(this)
+
+        customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+        customProgressDialog.show()
+    }
+
+    private fun showPermissionRationaleDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+    private fun requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showPermissionRationaleDialog(
+                "Kids Drawing App",
+                "This app requires permission to read ext storage to load an image from the gallery"
+            )
+        } else {
+            requestPermission.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
         }
     }
 }
